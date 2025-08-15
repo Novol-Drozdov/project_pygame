e142125d12416b9fc78e765583cf8f49ba51d683
Jo Sanders <Yaroslav.drozdov.07@list.ru>
2025-08-15 17:38:06 +0300

    Update Google Dino

--- Google_Dino/main.py
+++ Google_Dino/main.py
@@ -8,6 +8,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 imgSprites = pygame.image.load('project_pygame/Google_Dino/sprites.png').convert_alpha()
+imgBG = imgSprites.subsurface(2, 104, 2400, 26)
 imgDinoStand = [imgSprites.subsurface(1514, 2, 88, 94),
                 imgSprites.subsurface(1602, 2, 88, 94)]
 imgDinoSit = [imgSprites.subsurface(1866, 36, 118, 60),
@@ -16,7 +17,10 @@ imgDinoLose = [imgSprites.subsurface(1690, 2, 88, 94)]
 
 py, sy = 380, 0
 isStand = False
+speed = 200
 frame = 0
+
+bgs = [pygame.Rect(0, HEIGHT - 50, 2400, 26)]
 
 play = True
 while play:
@@ -30,7 +34,7 @@ while play:
     pressSit = keys[pygame.K_LCTRL] or keys[pygame.K_s] or keys[pygame.K_DOWN] or b3
 
     if pressJump and isStand: sy = -22
-    if isStand: frame = (frame + 0.2) % 2
+    if isStand: frame = (frame + speed / 35) % 2
 
     py += sy
     sy = (sy + 1) * 0.97
@@ -45,7 +49,17 @@ while play:
     dw, dh = dinoImage.get_width(), dinoImage.get_height()
     dinoRect = pygame.Rect(150, py - dh, dw, dh)
 
+    for i in range(len(bgs)-1, -1, -1):
+        bg = bgs[i]
+        bg.x -= speed
+
+        bg.right < 0 and bgs.pop(i)
+
+    if bgs[-1].right < WIDTH:
+        bgs.append(pygame.Rect(bgs[-1].right, HEIGHT - 50, 2400, 26))
+
     window.fill('white')
+    for bg in bgs: window.blit(imgBG, bg)
     window.blit(dinoImage, dinoRect)
 
     pygame.display.update()
